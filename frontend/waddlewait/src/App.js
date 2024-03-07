580bea0a3b2c8ff8547a5afb7d24e47527ec31fb
RyanKahingLee <z5362005@ad.unsw.edu.au>
2024-03-07 12:20:16 +1100

    screen template

--- frontend/waddlewait/src/App.js
+++ frontend/waddlewait/src/App.js
@@ -2,14 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StaffLogin from './Screens/Accounts/Login';
+import CustomerTableSelection from './Screens/Customer/TableSelection';
+import CustomerHomeMenu from './Screens/Customer/HomeMenu';
+import StaffAccountCreation from './Screens/Accounts/CreateAccount';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home/>} />
-        <Route path="/customer/table-selection" element={<customerTableSelection/>} />
-        <Route path="/login" element={<Login/>} />
+
+        {/* Staff login/creation */}
+        <Route path="/staff/login" element={<StaffLogin/>} />
+        <Route path="/staff/creation" elemnt={<StaffAccountCreation/>} />
+
+        {/* Customer Routes */}
+        <Route path="/customer/table-selection" element={<CustomerTableSelection/>} />
+        <Route path="/customer/home-menu" element={<CustomerHomeMenu/>} />
+
+
       </Routes>
     </BrowserRouter>
   );
@@ -25,48 +37,6 @@ function Home() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           EditT <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-function customerTableSelection() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          EddaadsdasitT <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-function Login() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is Login <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
