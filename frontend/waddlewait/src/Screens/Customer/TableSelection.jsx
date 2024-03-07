d0d9bec64919e63d2d26ec82439d6b55a6c49dbb
RyanKahingLee <z5362005@ad.unsw.edu.au>
2024-03-07 12:41:09 +1100

    fixed issue

--- frontend/waddlewait/src/Screens/Customer/TableSelection.jsx
+++ frontend/waddlewait/src/Screens/Customer/TableSelection.jsx
@@ -1,28 +1,26 @@
-// import * as React from 'react';
-// import Button from '@mui/material/Button';
-// import MenuItem from '@mui/material/MenuItem';
-// import NumberInput from '@mui/material/NumberInput';
-// import Select from '@mui/material/Select';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 
-// function TableSelection() {
-//     return (
-//         <>
-//             <h1>
-//                 Table Selection
-//             </h1>
-//             <NumberInput min={1} max={10} />
+function TableSelection() {
+    return (
+        <>
+            <h1>
+                Table Selection
+            </h1>
 
-//             <Select label="Table Number">
-//                 <MenuItem value={1}>1</MenuItem>
-//                 <MenuItem value={2}>2</MenuItem>
-//                 <MenuItem value={3}>3</MenuItem>
-//             </Select>
+            <Select label="Table Number">
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+            </Select>
 
-//             <Button variant="contained">
-//                 Confirm
-//             </Button>
-//         </>
-//     );
-// }
+            <Button variant="contained">
+                Confirm
+            </Button>
+        </>
+    );
+}
     
-// export default TableSelection
+export default TableSelection
