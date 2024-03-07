65acfea7ad7bbb8576f81e79c758dc26aad1a9aa
040 <83402259+040-Not-Found@users.noreply.github.com>
2024-03-07 12:08:29 +1100

    add components about costumer table selection

---
+++ frontend/waddlewait/src/Components/Customer.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function tableSelection() {
+    return (
+        <>
+            <h1>
+                Table Selection
+            </h1>
+
+            <Button variant="contained">
+                Confirm
+            </Button>
+        </>
+    );
+}
+    
