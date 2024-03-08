f82b12823ac7b28fed4fb5ac4e8798f6c67b827c
040 <83402259+040-Not-Found@users.noreply.github.com>
2024-03-08 20:10:46 +1100

    done table selection

--- frontend/waddlewait/src/Screens/Customer/TableSelection.jsx
+++ frontend/waddlewait/src/Screens/Customer/TableSelection.jsx
@@ -1,68 +1,101 @@
 import * as React from 'react';
+import './CustomerStyle.css';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
-import './CustomerStyle.css';
+import FormControl from '@mui/material/FormControl';
 import Grid from '@mui/material/Grid';
+import InputLabel from '@mui/material/InputLabel';
 import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
-import IconButton from '@mui/material/IconButton';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import Alert from '@mui/material/Alert';
 
 function TableSelection() {
-//     const [groupSize, setgroupSize] = useState(0);
+  const [groupSize, setGroupSize] = React.useState('')
+  const [tableNum, setTableNum] = React.useState('')
+  const [showError, setShowError] = React.useState(false)
 
-//   const handleIncrement = () => {
-//     setValue(groupSize + 1);
-//   };
+  const changeGroupSize = (event) => {
+    setGroupSize(event.target.value);
+  };
 
-//   const handleDecrement = () => {
-//     setValue(groupSize - 1);
-//   };
-    return (
+  const changeTableNum = (event) => {
+    setTableNum(event.target.value);
+  };
+
+  const handleConfirm = () => {
+    console.log(groupSize, tableNum);
+
+    if (tableNum && groupSize && groupSize > 0) {
+      console.log("confirmed!");
+      // Additional logic or navigation can be added here
+    } else {
+      // Set showError to true to render the error alert
+      setShowError(true);
+    }
+  };
+
+  return (
+    <>
+      <h1 >
+        Table Selection
+      </h1>
+
+      <Grid container spacing={5} style={{ justifyContent: 'center' }}>
+        <Grid item>
+          <FormControl fullWidth>
+            <TextField
+              id="groupSizeField"
+              label="Group Size"
+              type="number"
+              value={groupSize}
+              onChange={changeGroupSize}
+              sx={{ width: 150 }}
+            />
+          </FormControl>
+        </Grid>
+
+        <Grid item>
+            <FormControl fullWidth>
+              <InputLabel id="groupSizeLabel">Table Number</InputLabel>
+              <Select 
+              labelId="tableNumField"
+              label="Table Number" 
+              type="number"
+              value={tableNum}
+              onChange={changeTableNum}
+              sx={{ width: 150 }}
+            >
+              <MenuItem value={1}>1</MenuItem>
+              <MenuItem value={2}>2</MenuItem>
+              <MenuItem value={3}>3</MenuItem>
+              <MenuItem value={4}>4</MenuItem>
+              <MenuItem value={5}>5</MenuItem>
+              <MenuItem value={6}>6</MenuItem>
+              <MenuItem value={7}>7</MenuItem>
+              <MenuItem value={8}>8</MenuItem>
+              <MenuItem value={9}>9</MenuItem>
+            </Select>
+            </FormControl>
+        </Grid>
+      </Grid>
+
+      <br />
+
+      <Button variant="contained" onClick={handleConfirm} className="custom-button" color='warning' >
+        Confirm
+      </Button>
+
+      <br /><br />
+
+      {showError && (
         <>
-            <h1 >
-                Table Selection
-            </h1>
-
-            <Grid container spacing={5} style={{ justifyContent: 'center' }}>
-                <Grid item>
-                <TextField
-                label="Number"
-                type="number"
-                // value={value}
-                // onChange={(e) => setValue(e.target.value)}
-                InputProps={{
-                    endAdornment: (
-                    <InputAdornment position="end">
-                        <IconButton>
-                        <ArrowUpwardIcon />
-                        </IconButton>
-                        <IconButton>
-                        <ArrowDownwardIcon />
-                        </IconButton>
-                    </InputAdornment>
-                    ),
-                }}
-                />
-                </Grid>
-                <Grid item>
-                    <Select label="Table Number">
-                        <MenuItem value={1}>1</MenuItem>
-                        <MenuItem value={2}>2</MenuItem>
-                        <MenuItem value={3}>3</MenuItem>
-                    </Select>
-                </Grid>
-            </Grid>
-
-            <br />
-
-            <Button variant="contained">
-                Confirm
-            </Button>
+          <Alert severity="error" sx={{ width: '100%' }}>
+            Please fill in the Group Size and Table Number correctly.
+          </Alert>
         </>
-    );
+      )}
+    </>
+  );
 }
     
 export default TableSelection
