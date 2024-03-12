2e7c706fe924f791f79b581fc5e8a19d1c422365
Yijun Zou <yijun.zou@student.unsw.edu.au>
2024-03-12 16:21:04 +1100

    ok

---
+++ frontend/waddlewait/src/Screens/Kitchen/KitchenOrderRequests.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function HelloWorld() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/kitchen/order-requests')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+        <Button onClick={toggleDrawer(true)}>Open drawer</Button>
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+            { <KitchenSidebar />}
+        </Drawer>
+        <h1>Order Requests</h1>
+        <p>{message}</p>
+    </div>
+  );
+}
+
+export default HelloWorld;
