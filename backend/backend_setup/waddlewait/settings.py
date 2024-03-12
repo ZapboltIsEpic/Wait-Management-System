5ad48dadee8e76e1af4bf0717f7b35e81f834917
Dobro2910 <90589894+Dobro2910@users.noreply.github.com>
2024-03-12 16:12:52 +1100

    update auth backend

--- backend/backend_setup/waddlewait/settings.py
+++ backend/backend_setup/waddlewait/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres', 
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'depzai2910',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
