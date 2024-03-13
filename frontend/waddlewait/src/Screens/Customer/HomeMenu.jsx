5199c09ba3bbb32c38486b672a5618578a536d4b
040 <83402259+040-Not-Found@users.noreply.github.com>
2024-03-13 19:48:19 +1100

    add to cart notification

--- frontend/waddlewait/src/Screens/Customer/HomeMenu.jsx
+++ frontend/waddlewait/src/Screens/Customer/HomeMenu.jsx
@@ -9,7 +9,8 @@ import {
   CardActions,
   CardContent,
   CardMedia,
-  Grid
+  Grid,
+  Snackbar
 } from '@mui/material';
 import {
   TabList,
@@ -22,29 +23,59 @@ import {
 
 
 function Item({ item }) {
+  console.log(item.source)
+  const [showNotification, setShowNotification] = React.useState(false);
+
+  const handleAddToCart = () => {
+    setShowNotification(true);
+    setTimeout(() => {
+      setShowNotification(false);
+    }, 2000);
+  };
+
   return (
     <Card sx={{ width: 350, height: 300 }}>
       <CardMedia
         sx={{ height: 140 }}
-        src={item.source}
+        image={item.source}
         title={item.name}
       />
       <CardContent>
         <Typography gutterBottom>
           {item.name}
         </Typography>
-        <Typography color="text.secondary" sx={{ height: 50 }} >
+        <Typography variant="body2" color="text.secondary" sx={{ height: 50 }} >
           {item.description}
         </Typography>
       </CardContent>
       <CardActions>
+      <Box 
+        sx={{ 
+          display: 'flex', 
+          justifyContent: 'space-between', 
+          alignItems: 'center', 
+          width: '100%' 
+        }}
+      >
+        <Typography variant="body1" color="textSecondary">
+          $ {item.price.toFixed(2)}
+        </Typography>
         <Button 
           size="small" 
           color='warning'
+          endIcon={<ShoppingCart />}
+          onClick={handleAddToCart}
         >
-          Add to Order
+          Add
         </Button>
+      </Box>
       </CardActions>
+      <Snackbar
+      open={showNotification}
+      message="Item added to cart"
+      autoHideDuration={3000} // Hide notification after 3 seconds
+      onClose={() => setShowNotification(false)}
+      />
     </Card>
   );
 }
@@ -54,26 +85,30 @@ function itemExample() {
 
   const item1 = {};
   item1.name = "Curry"
-  item1.source = "/Users/a040/Desktop/COMP3900/capstone-project-3900h11a_chunkypenguins/frontend/waddlewait/src/Screens/imgs/curry.png"
-  item1.description = "description 1"
+  item1.source = "/Screens/imgs/curry.png"
+  item1.description = "description 1description 1description 1description 1description 1description 1description 1description "
+  item1.price = 15.95
   items.push(item1)
 
   const item2 = {};
   item2.name = "CurryChicken"
-  item2.source = "/Users/a040/Desktop/COMP3900/capstone-project-3900h11a_chunkypenguins/frontend/waddlewait/src/Screens/imgs/curry.png"
+  item2.source = ""
   item2.description = "description 2"
+  item2.price = 15
   items.push(item2)
 
   const item3 = {};
   item3.name = "CurryBeef"
-  item3.source = "/Users/a040/Desktop/COMP3900/capstone-project-3900h11a_chunkypenguins/frontend/waddlewait/src/Screens/imgs/curry.png"
+  item3.source = ""
   item3.description = "description 3"
+  item3.price = 15
   items.push(item3)
 
   const item4 = {};
   item4.name = "CurryBeef"
-  item4.source = "/Users/a040/Desktop/COMP4900/capstone-project-4900h11a_chunkypenguins/frontend/waddlewait/src/Screens/imgs/curry.png"
+  item4.source = ""
   item4.description = "description 4"
+  item4.price = 15
   items.push(item4)
 
   return items
@@ -98,13 +133,15 @@ function HomeMenu() {
       <h1>
         Welcome! Please select your meal!
 
-        <Button
-          variant="contained"
-          color='warning'
-        >
-          Cart 
-          <ShoppingCart />
-        </Button>
+        <Box textAlign='auto'>
+          <Button
+            variant="contained"
+            color='warning'
+            endIcon={<ShoppingCart />}
+            >
+            Cart 
+          </Button>
+        </Box>
       </h1>
 
       <TabContext value={value}>
