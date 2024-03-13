3aca52a48f0703c34e36374c223c68dc1b748b08
040 <83402259+040-Not-Found@users.noreply.github.com>
2024-03-13 23:38:13 +1100

    categories items to their categories

--- frontend/waddlewait/src/Screens/Customer/HomeMenu.jsx
+++ frontend/waddlewait/src/Screens/Customer/HomeMenu.jsx
@@ -32,7 +32,6 @@ import {
   ShoppingCart
 } from '@mui/icons-material';
 
-
 function Item({ item }) {
   const [showNotification, setShowNotification] = React.useState(false);
 
@@ -47,7 +46,7 @@ function Item({ item }) {
     <Card sx={{ width: 350, height: 300 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image={item.source}
+        image={item.image}
         title={item.name}
       />
       <CardContent>
@@ -96,6 +95,29 @@ function Item({ item }) {
       </Snackbar>
     </Card>
   );
+}
+
+function Items({ items, cateId }) {
+  var cateItems = [];
+
+  for (var index in items) {
+    var item = items[index];
+    if (item.category.id === cateId) {
+        cateItems.push(item);
+    }
+}
+
+  return (
+    <>
+      <Grid container spacing={4} justifyContent="center" alignItems="center">
+        {cateItems.map((item, index) => (
+          <Grid item key={index}>
+            <Item item={item} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  )
 }
 
 function Cart({ showCart, setShowCart, setBill }) {
@@ -161,33 +183,47 @@ function Cart({ showCart, setShowCart, setBill }) {
 function itemExample() {
   var items = []
 
+  // {"id", "name", "description", "price",”image” "category": { "id", "name"} } 
+
   const item1 = {};
   item1.name = "Curry"
-  item1.source = "https://www.allrecipes.com/thmb/FL-xnyAllLyHcKdkjUZkotVlHR8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/46822-indian-chicken-curry-ii-DDMFS-4x3-39160aaa95674ee395b9d4609e3b0988.jpg"
+  item1.image = "https://www.allrecipes.com/thmb/FL-xnyAllLyHcKdkjUZkotVlHR8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/46822-indian-chicken-curry-ii-DDMFS-4x3-39160aaa95674ee395b9d4609e3b0988.jpg"
   item1.description = "description 1description 1description 1description 1description 1description 1description 1description "
   item1.price = 15.95
+  item1.category = { "id":0, "name":"Appetizers"}
   items.push(item1)
 
   const item2 = {};
-  item2.name = "CurryChicken"
-  item2.source = "https://www.allrecipes.com/thmb/FL-xnyAllLyHcKdkjUZkotVlHR8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/46822-indian-chicken-curry-ii-DDMFS-4x3-39160aaa95674ee395b9d4609e3b0988.jpg"
+  item2.name = "Curry Chicken"
+  item2.image = "https://www.allrecipes.com/thmb/FL-xnyAllLyHcKdkjUZkotVlHR8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/46822-indian-chicken-curry-ii-DDMFS-4x3-39160aaa95674ee395b9d4609e3b0988.jpg"
   item2.description = "description 2"
-  item2.price = 15
+  item2.price = 15.95
+  item2.category = { "id":0, "name":"Appetizers"}
   items.push(item2)
 
   const item3 = {};
-  item3.name = "CurryBeef"
-  item3.source = "https://www.allrecipes.com/thmb/FL-xnyAllLyHcKdkjUZkotVlHR8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/46822-indian-chicken-curry-ii-DDMFS-4x3-39160aaa95674ee395b9d4609e3b0988.jpg"
+  item3.name = "Curry Beef"
+  item3.image = "https://www.allrecipes.com/thmb/FL-xnyAllLyHcKdkjUZkotVlHR8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/46822-indian-chicken-curry-ii-DDMFS-4x3-39160aaa95674ee395b9d4609e3b0988.jpg"
   item3.description = "description 3"
-  item3.price = 15
+  item3.price = 15.95
+  item3.category = { "id":0, "name":"Appetizers"}
   items.push(item3)
 
   const item4 = {};
-  item4.name = "CurryBeef"
-  item4.source = "https://www.allrecipes.com/thmb/FL-xnyAllLyHcKdkjUZkotVlHR8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/46822-indian-chicken-curry-ii-DDMFS-4x3-39160aaa95674ee395b9d4609e3b0988.jpg"
+  item4.name = "Curry Seafood"
+  item4.image = "https://www.allrecipes.com/thmb/FL-xnyAllLyHcKdkjUZkotVlHR8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/46822-indian-chicken-curry-ii-DDMFS-4x3-39160aaa95674ee395b9d4609e3b0988.jpg"
   item4.description = "description 4"
-  item4.price = 15
+  item4.price = 18.95
+  item4.category = { "id":0, "name":"Appetizers"}
   items.push(item4)
+
+  const item5 = {};
+  item5.name = "Ice Cream"
+  item5.image = "https://www.allrecipes.com/thmb/FL-xnyAllLyHcKdkjUZkotVlHR8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/46822-indian-chicken-curry-ii-DDMFS-4x3-39160aaa95674ee395b9d4609e3b0988.jpg"
+  item5.description = "description 5"
+  item5.price = 15
+  item5.category = { "id":1, "name":"Mains"}
+  items.push(item5)
 
   return items
 }
@@ -264,24 +300,19 @@ function HomeMenu() {
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList value={value} 
             onChange={handleChangeTab}
-            backgroundColor="warning"
+            backgroundcolor="warning"
           >
             {cates.map((cate, index) => (
               <Tab label={cate} value={index} />
             ))}
           </TabList>
         </Box>
-        {cates.map((cate, cIndex) => (
-          <TabPanel value={cIndex} key={cIndex}>
-            <Grid container spacing={4} justifyContent="center" alignItems="center">
-              {items.map((item, index) => (
-                <Grid item key={index}>
-                  <Item item={item} />
-                </Grid>
-              ))}
-            </Grid>
+        {cates.map((cate, index) => (
+          <TabPanel value={index} key={index}>
+            <Items items={items} cateId={index} />
           </TabPanel>
         ))}
+
       </TabContext>
     </>
   );
