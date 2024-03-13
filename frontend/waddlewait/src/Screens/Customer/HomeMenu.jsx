d2a59c51caceac2d066222fd4ec6d4ae6f4e0873
040 <83402259+040-Not-Found@users.noreply.github.com>
2024-03-13 14:14:29 +1100

    updates menu card

--- frontend/waddlewait/src/Screens/Customer/HomeMenu.jsx
+++ frontend/waddlewait/src/Screens/Customer/HomeMenu.jsx
@@ -6,6 +6,40 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
+
+function Items({ item }) {
+  console.log(item)
+  return (
+    <Card sx={{ width: 350, height: 300 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        src={item.source}
+        title={item.name}
+      />
+      <CardContent>
+        <Typography gutterBottom>
+          {item.name}
+        </Typography>
+        <Typography color="text.secondary" sx={{ height: 50 }} >
+          Ingredient of Curry
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button 
+          size="small" 
+          color='warning'
+        >
+          Add to Order
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -41,18 +75,22 @@ function a11yProps(index) {
 }
 
 function HomeMenu() {
-  const [value,setValue] = React.useState('')
-  
+  const [value, setValue] = React.useState(0)
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const item = {};
+  item.name = "Curry"
+  item.source = "/Users/a040/Desktop/COMP3900/capstone-project-3900h11a_chunkypenguins/frontend/waddlewait/src/Screens/imgs/curry.png"
 
   return (
     <>
       <h1>
         Welcome! Please select your meal!
 
-          <Button
+        <Button
           variant="contained"
           // endIcon={<ShoppingCartIcon />}
         >
@@ -68,7 +106,8 @@ function HomeMenu() {
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
-        Item One
+        {/* Render Items as a component */}
+        <Items item={item} />
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
         Item Two
@@ -77,7 +116,6 @@ function HomeMenu() {
         Item Three
       </CustomTabPanel>
     </>
-
   );
 }
 
