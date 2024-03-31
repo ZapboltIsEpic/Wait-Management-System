79bfc496410b8050842159cde71db9006b939a2e
Yijun Zou <yijun.zou@student.unsw.edu.au>
2024-03-31 18:23:00 +1100

    added columns for orders

--- frontend/waddlewait/src/Screens/Waiter/WaiterAssistanceRequests.jsx
+++ frontend/waddlewait/src/Screens/Waiter/WaiterAssistanceRequests.jsx
@@ -5,6 +5,74 @@ import { Button, Drawer } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import { WaiterSidebar } from './layout/WaiterSidebar';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Typography from '@mui/material/Typography';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+
+function MakeOrder() {
+  const [status, setStatus] = useState("Not Started");
+
+  const handleStatus = (event) => {
+    setStatus(event.target.value);
+  };
+
+  return (
+    <Card className="order-card" sx={{ minWidth: 300, maxHeight: 400, maxWidth: 300}}>
+      <CardHeader
+        title="Order num"
+        subheader="Table no"
+      />
+      <CardContent className="order-card-contents">
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Order details
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Salmon, Tuna, Sashimi
+        </Typography>
+        <Typography color="text.secondary">
+          Notes
+        </Typography>
+        <FormControl fullWidth>
+          <Button 
+            variant="contained" 
+            color="primary"
+            onClick={() => {
+              axios.put('http://localhost:8000/assistance/notifications/accepted', {
+                "tableNumber": 1, 
+                "staffName": "Bob", 
+                "tableStatus": false, 
+              })
+              .then(response => {
+                  // Handle response
+              })
+              .catch(error => {
+                  // Handle error
+        });
+            }}
+        >
+            Accept
+        </Button>
+        </FormControl>
+        {status === 'Ready' && (
+        <Button 
+          variant="contained" 
+          color="primary"
+          onClick={() => {
+            
+          }}
+        >
+          Close
+        </Button>
+      )}
+      </CardContent>
+    </Card>
+  )
+}
 
 function WaiterAssistanceRequests() {
   const [orderRequests, setOrderRequests] = useState([]);
@@ -34,8 +102,11 @@ function WaiterAssistanceRequests() {
 	};
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/waiter/order-requests')
+    axios.get('http://localhost:8000/assistance/requests')
       .then(response => {
+        response.data.forEach(item => {
+          console.log(item.tableNumber, item.staffName, item.tableStatus);
+        });
         setOrderRequests(response.data.table_number);
       })
       .catch(error => {
@@ -49,8 +120,17 @@ function WaiterAssistanceRequests() {
         <Drawer open={open} onClose={toggleDrawer(false)}>
             { <WaiterSidebar />}
         </Drawer>
-        <h1>Order Requests</h1>
+        <h1>Assistance Requests</h1>
         <p>{orderRequests}</p>
+        <div className="kitchen-orders-container">
+          {MakeOrder()}
+          {MakeOrder()}
+          {MakeOrder()}
+          {MakeOrder()}
+          {MakeOrder()}
+          {MakeOrder()}
+          {MakeOrder()}
+        </div>
     </div>
   );
 }
--- frontend/waddlewait/src/Screens/Waiter/WaiterOrderRequests.jsx
+++ frontend/waddlewait/src/Screens/Waiter/WaiterOrderRequests.jsx
@@ -8,6 +8,72 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogActions from '@mui/material/DialogActions';
 import { WaiterSidebar } from './layout/WaiterSidebar';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Typography from '@mui/material/Typography';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+
+function MakeOrder({ orderRequest }) {
+
+  const handleAcceptRequest = () => {
+    axios.put('http://localhost:8000/orders/delivernotifications/accepted', {
+      "table": orderRequest.table, 
+      "items": orderRequest.items, 
+      "ready_to_serve": orderRequest.ready_to_serve, 
+      "is_complete": orderRequest.is_complete, 
+      // should be the waiter's name
+      "wait_staff_assigned": "yijun", 
+      "deliver": orderRequest.deliver, 
+      "bill": orderRequest.bill, 
+    })
+    .then(response => {
+      console.log(response.json);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+    useNavigate("/waiter/order-request");
+  }
+
+  // or orderRequest.wait_staff_assigned == is not me
+  if (orderRequest.is_complete) {
+    return null;
+  }
+  return (
+    <Card className="order-card" sx={{ minWidth: 300, maxHeight: 400, maxWidth: 300}}>
+      <CardHeader
+        title={"Table no" + orderRequest.table}
+      />
+      <CardContent className="order-card-contents">
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Order details
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {orderRequest.items}
+        </Typography>
+        <Typography color="text.secondary">
+          Notes
+        </Typography>
+        <FormControl fullWidth>
+          <Button 
+            variant="contained" 
+            color="primary"
+            onClick={() => {
+              handleAcceptRequest();
+            }}
+        >
+          Accept
+        </Button>
+        </FormControl>
+      </CardContent>
+    </Card>
+  )
+}
 
 function WaiterOrderRequests() {
   const [orderRequests, setOrderRequests] = useState([]);
@@ -16,59 +82,63 @@ function WaiterOrderRequests() {
 
   const [shouldContinue, setShouldContinue] = useState(false);
 
-    useEffect(() => {
-        const getAlerts = () => {
-            if (!shouldContinue) {
-                return;
-            }
-            axios.get('http://localhost:8000/api/waiter/order-requests-alerts')
-                .then(response => {
-                    setAlerts(response.data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        }
-    
-        getAlerts(); 
-        const interval = setInterval(getAlerts, 1000); 
-        if (alerts.length > 0) {
-            setShouldContinue(false);
-        }
-    
-        return () => {
-            clearInterval(interval);
-        }
-    }, [])
+  // alerts not implemented yet for other users
+  // useEffect(() => {
+  //     const getAlerts = () => {
+  //         if (!shouldContinue) {
+  //             return;
+  //         }
+  //         axios.get('http://localhost:8000/api/waiter/order-requests-alerts')
+  //             .then(response => {
+  //                 setAlerts(response.data);
+  //             })
+  //             .catch(error => {
+  //                 console.log(error);
+  //             });
+  //     }
+  
+  //     getAlerts(); 
+  //     const interval = setInterval(getAlerts, 1000); 
+  //     if (alerts.length > 0) {
+  //         setShouldContinue(false);
+  //     }
+  
+  //     return () => {
+  //         clearInterval(interval);
+  //     }
+  // }, [])
 
 	const [openDrawer, setOpenDrawer] = React.useState(false);
 
-    const [openAlert, setOpenAlert] = React.useState(false);
+  const [openAlert, setOpenAlert] = React.useState(false);
 
 	const toggleDrawer = (isOpen) => () => {
 		setOpenDrawer(isOpen);
 	};
 
-    const handleClose = () => {
-        setOpenAlert(false);
-    };
+  const handleClose = () => {
+      setOpenAlert(false);
+  };
 
-//   useEffect(() => {
-//     axios.get('http://localhost:8000/api/waiter/order-requests')
-//       .then(response => {
-//         setOrderRequests(response.data.table_number);
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
-//   }, []);
+  useEffect(() => {
+    axios.get('http://localhost:8000/orders/requests')
+      .then(response => {
+        setOrderRequests(response.data);
+        response.data.forEach(item => {
+          console.log(item.table, item.items, item.ready_to_serve, item.is_complete, item.wait_staff_assigned, item.deliver, item.bill);
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
-    const addAlert = () => {
-        setAlerts({data: "New order request"});
-        console.log(alerts);
-        setOpenAlert(true);
-        console.log(alerts.length)
-    }
+  const addAlert = () => {
+      setAlerts({data: "New order request"});
+      console.log(alerts);
+      setOpenAlert(true);
+      console.log(alerts.length)
+  }
 
   return (
     <div className="App">
@@ -101,6 +171,11 @@ function WaiterOrderRequests() {
         </Drawer>
         <h1>Order Requests</h1>
         <p>{orderRequests}</p>
+        <div className="order-requests-container">
+          {orderRequests.map((request, index) => (
+            <MakeOrder key={index} orderRequest={request} />
+          ))}
+        </div>
     </div>
   );
 }
